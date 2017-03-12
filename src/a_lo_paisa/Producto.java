0d212e8c0d64271fd9249ffb16be70242676ce61
GONZALES <GONZALES@GONZALES-PC>
2017-03-12 13:18:11 -0500

    ejercicio

--- src/a_lo_paisa/Producto.java
+++ src/a_lo_paisa/Producto.java
@@ -10,5 +10,54 @@ package a_lo_paisa;
  * @author GONZALES
  */
 public class Producto {
+  private String nombre;
+  private int cantidad;
+  private ProductoEnt productoe;
+  private ProductoSal productos;
+
+    public Producto(String nombre, int cantidad, ProductoEnt productoe, ProductoSal productos) {
+        this.nombre = nombre;
+        this.cantidad = cantidad+productoe-productos;
+           }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public ProductoEnt getProductoe() {
+        return productoe;
+    }
+
+    public ProductoSal getProductos() {
+        return productos;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setProductoe(ProductoEnt productoe) {
+        this.productoe = productoe;
+    }
+
+    public void setProductos(ProductoSal productos) {
+        this.productos = productos;
+    }
     
+    
+    
+ public int totalProducto(){
+  int totalproducto= (this.getCantidad() +this.getProductoe());
+  return totalproducto;
+ }
+ 
+  
 }
