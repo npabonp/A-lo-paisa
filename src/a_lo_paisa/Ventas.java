7dd814e1ae627406698ea54f9c0867d8dd021846
jeisson <jeisson@10.203.170.133>
2017-03-14 17:24:04 -0500

    ZC

--- src/a_lo_paisa/Ventas.java
+++ src/a_lo_paisa/Ventas.java
@@ -5,12 +5,20 @@
  */
 package a_lo_paisa;
 
-
+/**
+*La clase Ventas permite conocer cuanto se vende en el restaurante diariamente, mensualmente, y anualmente, teniendo en cuenta los productos de salida del inventario.
+*Los get de la clase permiten obtener la informacion y los set permiten modificar los datos en caso de ser necesario.
+ */  
 public class Ventas {
-   
+ 
     private Producto [ ] productoo;
     private Fecha fecha;
-
+    
+/**
+ *El constructor Ventas permite recibir la cantidad de productos salientes y la fecha en que se realizo la venta.
+ * @param productoo recibe un arreglo con todos los productos salientes.
+ * @param fecha permite conocer la fecha en que se vendio el producto.
+ */
     public Ventas(Producto[] productoo, Fecha fecha) {
         this.productoo = productoo;
         this.fecha = fecha;
@@ -33,32 +41,46 @@ public class Ventas {
         this.fecha = fecha;
     }
 
-        
+     /**
+     *El metodo diario, permitira conecer cuantos productos se venden diariamente en el restaurante. 
+     *  @return Retona la venta total diaria del restaurante.
+     */        
             
     public double diario(){
     double diario=0;
     for(int j=0; j==this.getFecha().getDia();j++){
-    for(int i=0;i<this.productoo.length;i++){
-        diario+=this.getProductoo()[0].getCantidadS();
-    }}
+        for (Producto productoo1 : this.productoo) {
+            diario+=this.getProductoo()[0].getCantidadS();
+        }
+}
     return diario;
     } 
     
+     /**
+     *El metodo mensual, permitira conecer cuantos productos se venden mensualmente en el restaurante. 
+     *  @return Retona la venta total mensual del restaurante.
+     */   
     public double mensual(){
     double mensual=0;
     for(int j=0; j==this.getFecha().getMes();j++){
-    for(int i=0;i<this.productoo.length;i++){
-        mensual+=this.getProductoo()[0].getCantidadS();
-    }}
+        for (Producto productoo1 : this.productoo) {
+            mensual+=this.getProductoo()[0].getCantidadS();
+        }
+}
     return mensual;
     } 
+    /**
+     *El metodo anual, permitira conecer cuantos productos se venden anualmente en el restaurante. 
+     *  @return Retona la venta total anual del restaurante.
+     */   
     
      public double anual(){
     double anual=0;
     for(int j=0; j==this.getFecha().getAño();j++){
-    for(int i=0;i<this.productoo.length;i++){
-        anual+=this.getProductoo()[0].getCantidadS();
-    }}
+        for (Producto productoo1 : this.productoo) {
+            anual+=this.getProductoo()[0].getCantidadS();
+        }
+}
     return anual;
     } 
     
